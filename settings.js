02b89f7fc250797190e0354280e154e119e7fd5e
Arlo-dragony <arlodeadpool@gmail.com>
2024-07-22 12:20:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = true //auto block +92 
